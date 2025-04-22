3b71867b98c2c0efabc45f0b52f0b74eb3001ccb
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-22 11:49:59 +0200

    gfdgdg

--- cat.itb.NF3EA2_VelazquezLluc/Program.cs
+++ cat.itb.NF3EA2_VelazquezLluc/Program.cs
@@ -14,22 +14,94 @@ namespace cat.itb.NF3EA2_VelazquezLluc
         public StudentCRUD studentCRUD = new StudentCRUD();
 		public static void Main(string[] args)
         {
-			//LoadAllCollections();
-			//Exercice2a();
-			//Exercice2b();
-			//Exercice2c();
-			//Exercice2d("11219");
-			//Exercice2e();
-			//Exercice2f();
-			//Exercice2g();
-			//Exercice3a();
-			//Exercice3b();
-			//Exercice3c();
-			//Exercice3d();
-			//Exercice3e();
-			//Exercice3f();
-			//Exercice3g();
-			Exercice4a();
+			const string Msg = "0: Sortir \n1: Exercici1 \n2a: Exercici2a \n2b: Exercici2b \n2c: Exercici2c \n2d: Exercici2d " +
+				"\n2e: Exercici2e \n2f: Exercici2f \n2g: Exercici2g \n3a: Exercici3a \n3b: Exercici3b \n3c: Exercici3c \n3d: Exercici3d" +
+				"\n3e: Exercici3e \n3f: Exercici3f \n3g: Exercici3g \n4a: Exercici4a \n4b: Exercici4b \n4c: Exercici4c \n4d: Exercici4d " +
+				"\n4e: Exercici4e \n4f: Exercici4f \n4g: Exercici4g \n4h: Exercici4h \n5: Exercici5";
+			bool flag = true;
+			while (flag)
+			{
+				Console.WriteLine(Msg);
+				string num = Console.ReadLine();
+				switch (num)
+				{
+					case "1":
+						LoadAllCollections();
+						break;
+					case "2a":
+						Exercice2a();
+						break;
+					case "2b":
+						Exercice2b();
+						break;
+					case "2c":
+						Exercice2c();
+						break;
+					case "2d":
+						Exercice2d("11219");
+						break;
+					case "2e":
+						Exercice2e();
+						break;
+					case "2f":
+						Exercice2f();
+						break;
+					case "2g":
+						Exercice2g();
+						break;
+					case "3a":
+						Exercice3a();
+						break;
+					case "3b":
+						Exercice3b();
+						break;
+					case "3c":
+						Exercice3c();
+						break;
+					case "3d":
+						Exercice3d();
+						break;
+					case "3e":
+						Exercice3e();
+						break;
+					case "3f":
+						Exercice3f();
+						break;
+					case "3g":
+						Exercice3g();
+						break;
+					case "4a":
+						Exercice4a();
+						break;
+					case "4b":
+						Exercice4b();
+						break;
+					case "4c":
+						Exercice4c();
+						break;
+					case "4d":
+						Exercice4d();
+						break;
+					case "4e":
+						Exercice4e();
+						break;
+					case "4f":
+						Exercice4f();
+						break;
+					case "4g":
+						Exercice4g();
+						break;
+					case "4h":
+						Exercice4h();
+						break;
+					case "5":
+						Exercice5();
+						break;
+					default:
+						flag = false;
+						break;
+				}
+			}
 		}
 		public static void LoadAllCollections()
         {
@@ -120,6 +192,24 @@ namespace cat.itb.NF3EA2_VelazquezLluc
 		public static void Exercice4f()
 		{
 			ProductCRUD.DeleteProductsByCategory("phone");
+		}
+		public static void Exercice4g()
+		{
+			PeopleCRUD.RemoveTagsFromTeachers();
+		}
+		public static void Exercice4h()
+		{
+			CountryCRUD.DeleteCountriesByLanguage("Spanish");
+		}
+		public static void Exercice5()
+		{
+			GeneralCRUD.DropCollection("itb", "books");
+			GeneralCRUD.DropCollection("itb", "countries");
+			GeneralCRUD.DropCollection("itb", "grades");
+			GeneralCRUD.DropCollection("itb", "people");
+			GeneralCRUD.DropCollection("itb", "products");
+			GeneralCRUD.DropCollection("itb", "restaurants");
+			GeneralCRUD.DropCollection("itb", "students");
 		}
 	}
 }
---
+++ cat.itb.NF3EA2_VelazquezLluc/cruds/GeneralCRUD.cs
@@ -0,0 +1,31 @@
+﻿using cat.itb.NF3EA2_VelazquezLluc.connections;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cat.itb.NF3EA2_VelazquezLluc.cruds
+{
+    public static class GeneralCRUD
+    {
+		public static void DropCollection(string databaseName, string collectionName)
+		{
+			var database = MongoLocalConnection.GetDatabase(databaseName);
+			var collection = database.GetCollection<BsonDocument>(collectionName);
+			var documentCount = collection.CountDocuments(new BsonDocument());
+			Console.WriteLine($"Nombre de documents a la col·lecció '{collectionName}': {documentCount}");
+
+			database.DropCollection(collectionName);
+			Console.WriteLine($"La col·lecció '{collectionName}' ha estat eliminada.");
+			var remainingCollections = database.ListCollectionNames().ToList();
+			Console.WriteLine("Col·leccions restants a la base de dades:");
+			foreach (var remainingCollection in remainingCollections)
+			{
+				Console.WriteLine($"- {remainingCollection}");
+			}
+		}
+	}
+}
